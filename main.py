b8cddd89bb6920e825c3a0269aa60615f5589c60
Sourabh Bainsla <100192657+sourabh3624@users.noreply.github.com>
2023-06-20 06:45:25 +0530

    Add files via upload
     login page and dashboard page is done and edit button is also involve

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,session,redirect
 from flask_sqlalchemy import SQLAlchemy
 import json
 from datetime import datetime
@@ -12,7 +12,7 @@ local_server=True
 
 
 app = Flask(__name__, static_url_path='/static')
-
+app.secret_key = 'super-secret-key'
 '''app.config.update(
     MAIL_SERVER ="smtp.gmail.com",
     MAIL_PORT='465',
@@ -66,10 +66,20 @@ def about():
     return render_template("about.html",params=params)
 
 @app.route("/dashboard",methods=['GET','POST'])
+
 def dashboard():
+    if "user" in session and session['user']==params['admin_user']:
+        posts = Posts.query.all()
+        return render_template("dashboard.html", params=params, posts=posts)
+
     if request.method=="POST":
-        #REDIRECT TO ADMIN PANEL
-        pass
+        username = request.form.get("uname")
+        userpass = request.form.get("pass")
+        if username==params['admin_user'] and userpass==params['admin_password']:
+            # set the session variable
+            session['user']=username
+            posts = Posts.query.all()
+            return render_template("dashboard.html", params=params, posts=posts)
     else:
         return render_template("login.html", params=params)
 
@@ -88,9 +98,36 @@ def post_route(post_slug):
     post=Posts.query.filter_by(slug=post_slug).first()
     return render_template("post.html",params=params,post=post)
 '''
+@app.route("/edit/<string:sno>",methods=['GET','POST'])
+def edit(sno):
+    if "user" in session and session['user']==params['admin_user']:
+        if request.method =='POST':
+            box_title =request.form.get("title")
+            tline =request.form.get("tline")
+            slug =request.form.get("slug")
+            content =request.form.get("content")
+            img_file =request.form.get("img_file")
+            date= datetime.now()
 
 
+            if sno=="0":
+                post=Posts(title=box_title,slug=slug,content=content,tagline=tline,img_file=img_file,date=date)
+                db.session.add(post)
+                db.session.commit()
+            else:
 
+                post=Posts.query.filter_by(sno=sno).first()
+                post.title=box_title
+                post.slug=slug
+                post.content=content
+                post.tagline=tline
+                post.img_file=img_file
+                post.date=date
+                db.session.commit()
+                return redirect("/edit/"+sno)
+
+        post=Posts.query.filter_by(sno=sno).first()
+        return render_template("edit.html",params=params, post=post)
 
 @app.route("/contact",methods=['GET','POST'])
 def contact():
