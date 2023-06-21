5fb3220161bba530ab55ff4676e32df190356561
Sourabh Bainsla <100192657+sourabh3624@users.noreply.github.com>
2023-06-22 03:38:46 +0530

    Add files via upload
    pagination is done on the blog site...

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 from datetime import datetime
 import os
 from werkzeug.utils import secure_filename
+import math
 
 # from flask_mail import Mail
 
@@ -62,8 +63,32 @@ class Posts(db.Model):
     img_file=db.Column(db.String(20),nullable=False)
 @app.route("/")
 def home():
-    posts=Posts.query.filter_by().all()[0:params["no_of_posts"]]
-    return render_template("index.html",params=params,posts=posts)
+# pagination logic:
+    posts=Posts.query.filter_by().all()
+    last=math.ceil(len(posts)/int(params['no_of_posts']))
+    #[0:params["no_of_posts"]]
+    page=request.args.get('page')
+    if (not str(page).isnumeric()):
+            page=1
+    page=int(page)
+    posts = posts[(page-1)*int(params["no_of_posts"]): (page-1)*int(params["no_of_posts"]) + int(params["no_of_posts"])]      
+    if(page==1):
+            prev='#'
+            next="/?page="+ str(page + 1)
+    elif(page==last):
+            next='#'
+            prev="/?page="+ str(page - 1)
+    else:
+            prev="/?page="+ str(page - 1)
+            next="/?page="+ str(page + 1)
+
+
+
+
+
+
+    
+    return render_template("index.html",params=params,posts=posts,prev=prev,next=next)
 
 @app.route("/about")
 def about():
@@ -131,7 +156,7 @@ def edit(sno):
                 return redirect("/edit/"+sno)
 
         post=Posts.query.filter_by(sno=sno).first()
-        return render_template("edit.html",params=params, post=post)
+        return render_template("edit.html",params=params, post=post,sno=sno)
 @app.route("/logout")
 def logout():
     session.pop('user')
